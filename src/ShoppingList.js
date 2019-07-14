d827784dcd6c4c57d6444420d3ad58c1d3718e06
Dragos Nedelcu <dragosgn@gmail.com>
2019-07-14 12:30:31 +0200

    adding-state

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -1,7 +1,34 @@
 import React from "react";
+import "./styles.css";
 
-const ShoppingList = function({ day, hour, year }) {
-  return <p>I am the shopping list for {`${day} ${hour} ${year}`}</p>;
-};
+class ShoppingList extends React.Component {
+  state = {
+    bread: 8,
+    unitPrice: 2
+  };
+  render() {
+    const { day, hour, year } = this.props;
+    return (
+      <div>
+        <p>I am the shopping list for {`${day} ${hour} ${year}`}</p>
+        <div className="row">
+          <label for="bread">Panes:</label>
+          <input
+            onChange={e => {
+              console.log(e.target.value);
+            }}
+            className="input"
+            type="text"
+            name="bread"
+            id="bread"
+            placeholder="5.."
+            value={this.state.bread}
+          />
+        </div>
+        <p>Coste total: {this.state.bread * this.state.unitPrice}€</p>
+      </div>
+    );
+  }
+}
 
 export default ShoppingList;
